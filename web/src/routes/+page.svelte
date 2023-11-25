a4d58b68bd358c38b7d2741591d89f9250440eb7
TheWisePigeon <zozozozeph@gmail.com>
2023-11-25 16:53:50 +0000

    setup sveltekit project

---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	async function login() {
+		const response = await fetch('http://localhost:8080/auth/github');
+		if (response.status == 200) {
+			const { url } = (await response.json()) as { url: string };
+			goto(url)
+		}
+	}
+</script>
+
+<button on:click={login}>
+	<h1>Login</h1>
+</button>
