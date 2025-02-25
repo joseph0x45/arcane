980c12b7cb59c8d746e64306d875a522e9c33f1e
joseph0x45 <zozozozeph@gmail.com>
2025-02-25 18:02:09 +0000

    rename README and delete files in server

--- server/models/team.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Team struct {
-	Id    string `json:"id" db:"id"`
-	Name  string `json:"name" db:"id"`
-	Owner string `json:"owner" db:"owner"`
-	Plan  string `json:"plan" db:"plan"`
-}
--- server/models/user.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type User struct {
-	Id        string `json:"id" db:"id"`
-	GithubId  string `json:"github_id" db:"github_id"`
-	Email     string `json:"email" db:"email"`
-	Username  string `json:"username" db:"username"`
-	AvatarURL string `json:"avatar_url" db:"avatar_url"`
-}
